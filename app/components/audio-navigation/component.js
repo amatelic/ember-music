962aeff44a3d98c2d5426b027c2b253cfc41f2ff
amatelic <amatelic93@gmail.com>
2016-05-27 23:12:00 +0200

    Fixed bug with adding new playlist directories

--- app/components/audio-navigation/component.js
+++ app/components/audio-navigation/component.js
@@ -1,19 +1,36 @@
 import Ember from 'ember';
 import $ from 'jquery';
+let {run} = Ember;
+function hideContent(e) {
+  if (!$(e.target).hasClass('playlist__input')) {
+    this.set('addNewFile', false);
+    return;
+  }
+  //Fixe for if user first clicks on the input value
+  $('body').one('click', hideContent.bind(this));
+}
+
 export default Ember.Component.extend({
   addNewFile: false,
   classNames: ['audio__navigation'],
   actions: {
     addFolder() {
-      this.toggleProperty('addNewFile');
-      // code here will execute within a RunLoop in about 500ms with this == myContext
+      if (!this.get('addNewFile')) {
+        this.toggleProperty('addNewFile');
 
+        //Problem because element is stil not renderd have
+        //schedule task
+        run.scheduleOnce('afterRender', this, () => {
+          this.$('.playlist__input').focus();
+        });
+        $('body').one('click', hideContent.bind(this));
+      }
     },
 
     addNewFolder(e) {
       if (e.which === 13) {
-        this.toggleProperty('addNewFile');
         this.sendAction('createFolder', e.target.value);
+        $('body').click(); //DON'T REMOVE bug with removing event on body
         e.target.value = '';
       }
 
