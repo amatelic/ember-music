d772ce1ae2b750ce120479101d77ddca2b0b1906
amatelic <amatelic93@gmail.com>
2017-06-18 17:23:46 +0200

    Update style for login and register and updated validation

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -2,15 +2,30 @@ import Ember from 'ember';
 import $ from 'jquery';
 import swal from 'sweetalert';
 import { storageFor } from 'ember-local-storage';
+import _ from 'lodash';
+
 export default Ember.Controller.extend({
+  hasError: {
+    formIsEmpty: true,
+    isEmail: true,
+    isSamePassword: true
+  },
   storage: storageFor('user'),
   actions: {
     registerUser() {
-      let obj = this.getProperties('email', 'password', 'repassword');
+      let values = this.getProperties('email', 'password', 'repassword');
+
+      if (this.isCorrectValues(values)) {
+        return;
+      }
+
+      console.log('Send');
+      return;
+
       $.ajax({
         type: "POST",
         url: "http://localhost:5000/register",
-        data: obj,
+        data: values,
         success: function(res) {
           if (res.status === 404) {
             swal('Oops...', res.error, "error");
@@ -20,6 +35,29 @@ export default Ember.Controller.extend({
           }
         }.bind(this),
       });
+    },
+  },
+
+  isCorrectValues({email, password, repassword}) {
+    this.set('hasError.formIsEmpty', true);
+    this.set('hasError.isSamePassword', true);
+    this.set('hasError.isEmail', true);
+
+    if(_.isEmpty(email) || _.isEmpty(password) || _.isEmpty(repassword)) {
+      this.set('hasError.formIsEmpty', false);
+      return true;
     }
+
+    if(!/\S+@\S+\.\S+/.test(email)) {
+      this.set('hasError.isEmail', false);
+      return true;
+    }
+
+    if(password !== repassword) {
+      this.set('hasError.isSamePassword', false);
+      return true;
+    }
+
+    return false;
   }
 });
